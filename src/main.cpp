0ecc3066d65b01dd8ab3619645ef9595726ec80d
fnJustin <justin.sneddon@foundry.com>
2018-09-17 14:13:07 +0100

    adding valgrind and cpp check and sonarqube instructions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "math.h"
+int * leak()
+{
+    return new int();
+}
 
 int main(int argc, char** argv)
 {
 
     math::vec3 v(1,2,3);
+
+    int * l = leak();
     
     printf("Yo! %f \n",v.len());
     
